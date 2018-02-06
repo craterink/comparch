544350a4e9802f8638ff7e766dc9022c78340bcd
Garret E Blevins <gblevins@mario.ece.utexas.edu>
2018-02-05 18:51:00 -0600

    Testing complete.

--- lab2/test.asm
+++ lab2/test.asm
@@ -10,17 +10,20 @@
      LEA R0, func   ; R0 ->func
      JMP R0
      
-     LEA R0, val
+back LEA R0, val
      STB R2, R0, #2
-     STB R3, R1, #3
+     STB R3, R0, #3
      LEA R1, val1
      LDB R4, R1, #0 ; R4 = 0xAA
      LDB R5, R1, #1 ; R5 = 0xBB
+
+     TRAP x25
 
 func LEA R1, val    ; R1 = ->val
      LDB R2, R1, #0 ; R2 = 0xAA
      LDB R3, R1, #1 ; R3 = 0xBB
      LEA R1, func   ; R1 = ->func
+     LEA R7, back
      RET            ; PC = R7
 
 val  .fill xBBAA
